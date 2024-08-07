d310db70df2f2bc0d448fa8921f5736b0e3347a2
Hora-keven <kevensantos993@gmail.com>
2024-08-07 14:57:16 -0300

    feat: get server message using grpc communication

--- grpc_comunication_between_services/consuming_api.py
+++ grpc_comunication_between_services/consuming_api.py
@@ -3,25 +3,27 @@ import requests
 import grpc
 import service_pb2
 import service_pb2_grpc
+from google.protobuf.json_format import MessageToDict
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = service_pb2_grpc.MyServiceStub(channel)
-        response = stub.GetPerson(service_pb2.PersonRequest(id=1))
-        print(f"Name: {response.person.name}")
-        print(f"{response.person}")
+        response = stub.GetPerson(service_pb2.PersonRequest(id='Keven'))
+        new_format_response = MessageToDict(response)
         
-    return f"{response.person}"
+    return new_format_response
 
 
 
 app = Flask(__name__)
-SERIVCE_URL = '10.234.149.119:8000/api/v1'
 
-@app.route('/grpc_server_message', methods=['GET'])
-def service():
-    server_message = run()
-    return (server_message, 200)
+@app.route('/grpc_server_message', methods=['GET', "PUT","DELETE", "POST"])
+def get_server_message():
+    try:
+        server_message = run()
+        return jsonify(server_message), 200
+    except grpc.RpcError as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run("127.0.0.1",port=8080,debug=True)
