1252e4c4d22d825f5d0c32f704d12d20654ed055
Hora-keven <kevensantos993@gmail.com>
2024-08-07 15:21:42 -0300

    fix: default grpc port

--- grpc_comunication_between_services/consuming_api.py
+++ grpc_comunication_between_services/consuming_api.py
@@ -6,7 +6,7 @@ import service_pb2_grpc
 from google.protobuf.json_format import MessageToDict
 
 def run(search:str):
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('localhost:90090') as channel:
         stub = service_pb2_grpc.MyServiceStub(channel)
         response = stub.GetPerson(service_pb2.PersonRequest(search=search))
         new_format_response = MessageToDict(response)
