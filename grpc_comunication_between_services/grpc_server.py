bedb114cf3407ef86b7c4bb6d1468e8b34b50217
Hora-keven <kevensantos993@gmail.com>
2024-08-07 14:58:46 -0300

    feat: server receive client request and return the response through request

--- grpc_comunication_between_services/grpc_server.py
+++ grpc_comunication_between_services/grpc_server.py
@@ -3,10 +3,31 @@ from concurrent import futures
 import grpc
 import service_pb2
 import service_pb2_grpc
-
+from dataclasses import dataclass
+@dataclass
+class Person:
+    name:str
+    age:int
+    have_car:bool
+    
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+    def get_have_car(self):
+        return self.have_car
 class MyServiceServicer(service_pb2_grpc.MyServiceServicer):
     def GetPerson(self, request, context):
-        person = service_pb2.Person(name="Person 1",age=12, haveCar=13)
+        people:list[Person] = [Person("Keven",14,True),Person("Antonio",15,True), Person("Davi",11,False),Person("Rosimeire",36,True)]
+        person_choose = None
+        
+        for i in people:
+            print(i)
+            if i.get_name() == str(request.id):
+                person_choose = i
+                print("There is")
+                
+        person = service_pb2.Person(name=person_choose.get_name(),age=person_choose.get_age(), have_car=person_choose.get_have_car())
         return service_pb2.PersonResponse(person=person)
         
 
