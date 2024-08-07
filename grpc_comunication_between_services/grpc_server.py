c2c39a7bee7210d135bfe960b8d4a023dc22d004
Hora-keven <kevensantos993@gmail.com>
2024-08-07 15:23:24 -0300

    fix: default grpc port and change the param id to search

--- grpc_comunication_between_services/grpc_server.py
+++ grpc_comunication_between_services/grpc_server.py
@@ -16,14 +16,14 @@ class Person:
         return self.age
     def get_have_car(self):
         return self.have_car
+    
 class MyServiceServicer(service_pb2_grpc.MyServiceServicer):
     def GetPerson(self, request, context):
         people:list[Person] = [Person("Keven",14,True),Person("Antonio",15,True), Person("Davi",11,False),Person("Rosimeire",36,True)]
         person_choose = None
         
         for i in people:
-            print(i)
-            if i.get_name() == str(request.id):
+            if i.get_name() == str(request.search):
                 person_choose = i
                 print("There is")
                 
@@ -34,7 +34,7 @@ class MyServiceServicer(service_pb2_grpc.MyServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     service_pb2_grpc.add_MyServiceServicer_to_server(MyServiceServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:90090')
     server.start()
     server.wait_for_termination()
 
