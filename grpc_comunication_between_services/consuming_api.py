b1a2151cba15443a9aa60ac41eaeb9b5395850f5
Hora-keven <kevensantos993@gmail.com>
2024-08-07 15:15:49 -0300

    feat: search in server by params

--- grpc_comunication_between_services/consuming_api.py
+++ grpc_comunication_between_services/consuming_api.py
@@ -5,10 +5,10 @@ import service_pb2
 import service_pb2_grpc
 from google.protobuf.json_format import MessageToDict
 
-def run():
+def run(search:str):
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = service_pb2_grpc.MyServiceStub(channel)
-        response = stub.GetPerson(service_pb2.PersonRequest(id='Keven'))
+        response = stub.GetPerson(service_pb2.PersonRequest(search=search))
         new_format_response = MessageToDict(response)
         
     return new_format_response
@@ -17,10 +17,10 @@ def run():
 
 app = Flask(__name__)
 
-@app.route('/grpc_server_message', methods=['GET', "PUT","DELETE", "POST"])
-def get_server_message():
+@app.route('/grpc_server_message/<string:search>', methods=['GET', "PUT","DELETE", "POST"])
+def get_server_message(search:str):
     try:
-        server_message = run()
+        server_message = run(search)
         return jsonify(server_message), 200
     except grpc.RpcError as e:
         return jsonify({"error": str(e)}), 500
