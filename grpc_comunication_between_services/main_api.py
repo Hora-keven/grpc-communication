9b14ddec4aa367cee754b77cca5c92bbff6cea7f
Hora-keven <kevensantos993@gmail.com>
2024-08-08 10:45:24 -0300

    feat: do transaction method finished

--- grpc_comunication_between_services/main_api.py
+++ grpc_comunication_between_services/main_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request,jsonify
 import grpc
 import service_pb2
 import service_pb2_grpc 
@@ -9,17 +9,15 @@ def run(who_send:str, who_receive:str, value:float):
         stub = service_pb2_grpc.MyServiceStub(channel)
         response = stub.GetPerson(service_pb2.PersonRequest(who_send=who_send, who_receive=who_receive,value=value))
         new_format_response = MessageToDict(response)
-        
     return new_format_response
-
-
 
 app = Flask(__name__)
 
-@app.route('/transaction/<string:who_send>/<string:who_receive>/<float:value>', methods=['GET'])
-def get_transaction(who_send:str, who_receive:str,value:float):
+@app.route('/transaction', methods=['POST'])
+def do_transaction():
+    data = request.json
     try:
-        server_message = run(who_send=who_send,who_receive=who_receive,value=value)
+        server_message = run(who_send=data["who_send"],who_receive=data["who_receive"],value=data["value"])
         return jsonify(server_message), 200
     except grpc.RpcError as e:
         return jsonify({"error": str(e)}), 500
