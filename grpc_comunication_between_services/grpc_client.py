1b5317eba2d088428d54d8dd1be79a63b1e256a7
Hora-keven <kevensantos993@gmail.com>
2024-08-08 12:00:53 -0300

    style: print

--- grpc_comunication_between_services/grpc_client.py
+++ grpc_comunication_between_services/grpc_client.py
@@ -9,7 +9,7 @@ def run():
     with grpc.insecure_channel('localhost:90090') as channel:
         stub = service_pb2_grpc.MyServiceStub(channel)
         response = stub.GetPerson(service_pb2.PersonRequest(who_send="Keven",who_receive="Rosimeire",value=1100))
-        print(f"Name: {response.person_send}")
+        print(f"\nWHO SEND:\n{response.person_send}--------------------\nWHO RECEIVED:\n{response.person_receive}\nVALUE:{response.value}")
 
 if __name__ == '__main__':
     run()
