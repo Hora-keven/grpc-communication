4e2756fa1449c9518dde53fce864a3dbc0e53427
Hora-keven <kevensantos993@gmail.com>
2024-08-08 10:44:18 -0300

    feat: transaction finished

--- grpc_comunication_between_services/api.py
+++ grpc_comunication_between_services/api.py
@@ -1,17 +1,18 @@
 from dataclasses import dataclass
 from models import Person
+import grpc
  
+people:list[Person] = [Person("Keven",14,False,1234.45),Person("Antonio",15,True,100.1), Person("Davi",11,False,230.3),Person("Rosimeire",36,True,300000.0)]
+
 @dataclass
 class Api:
-    people:list[Person] = [Person("Keven",14,False,1234.45),Person("Antonio",15,True,100.1), Person("Davi",11,False,230.3),Person("Rosimeire",36,True,300000.0)]
-    
     
     def transaction(self, who_send:str, who_receive, value:float):
         try:
             who_send_founded = None
             who_receive_founded = None
             
-            for i in self.people:
+            for i in people:
                 if i.get_name() == who_send:
                     who_send_founded = i
                 else:
@@ -21,17 +22,27 @@ class Api:
                 else:
                     print("There is no who receive")
                     
-            who_send_founded.get_balance()-=value
-            who_receive_founded.get_balance()+=value
-            
-            data = {
-                "who_send":who_send_founded,
-                "who_receive":who_receive_founded,
-                "value":value
-            }
-            return data
+            if who_send_founded.get_balance() > value:   
+                who_send_founded.set_balance(round(who_send_founded.get_balance()-value,2))
+                who_receive_founded.set_balance(round(who_receive_founded.get_balance()+value,2))
+       
+                data = {
+                    "who_send":who_send_founded,
+                    "who_receive":who_receive_founded,
+                    "value":value,
+                    "message":"Transaction successfully!"
+                }
+                return data
+            else:
+                data = {
+                    "who_send":who_send_founded,
+                    "who_receive":who_receive_founded,
+                    "value":value,
+                    "message":f"You dont have money, your balance its {who_send_founded.get_balance()}"
+                }
+                return data
         except:
-            print("ERRRO")
+            raise grpc.RpcError()
         
             
             
