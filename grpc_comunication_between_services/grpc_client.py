47318d736241f50b7938e8c04079b7de4320ed25
Hora-keven <kevensantos993@gmail.com>
2024-08-08 10:44:47 -0300

    feat: converted grpc files

--- grpc_comunication_between_services/grpc_client.py
+++ grpc_comunication_between_services/grpc_client.py
@@ -2,14 +2,14 @@
 from concurrent import futures
 
 import grpc
-from . import service_pb2
-from . import service_pb2_grpc
+import service_pb2 
+import service_pb2_grpc
 
 def run():
     with grpc.insecure_channel('localhost:90090') as channel:
         stub = service_pb2_grpc.MyServiceStub(channel)
-        response = stub.GetPerson(service_pb2.PersonRequest(search="person name"))
-        print(f"Name: {response.person.name}")
+        response = stub.GetPerson(service_pb2.PersonRequest(who_send="Keven",who_receive="Rosimeire",value=1100))
+        print(f"Name: {response.person_send}")
 
 if __name__ == '__main__':
     run()
--- grpc_comunication_between_services/grpc_server.py
+++ grpc_comunication_between_services/grpc_server.py
@@ -1,20 +1,21 @@
 # server.py
 from concurrent import futures
 import grpc
-from . import service_pb2
-from . import  service_pb2_grpc
+import service_pb2
+import  service_pb2_grpc
 from api import Api
 
     
 class MyServiceServicer(service_pb2_grpc.MyServiceServicer):
     def GetPerson(self, request, context):
+        print(request)
         api = Api()
         person_choose = api.transaction(request.who_send,request.who_receive,request.value)
-
-        person_send = service_pb2.Person(**person_choose.get("who_send"))
-        person_receive = service_pb2.Person(**person_choose.get("who_receive"))
-        return service_pb2.PersonResponse(person_send=person_send,person_receive=person_receive,value=request.value)
-        
+      
+        person_send = service_pb2.Person(name=person_choose.get("who_send").get_name(),age=person_choose.get("who_send").get_age(),balance=person_choose.get("who_send").get_balance(),have_debts=person_choose.get("who_send").get_have_debts())
+        person_receive = service_pb2.Person(name=person_choose.get("who_receive").get_name(),age=person_choose.get("who_receive").get_age(),balance=person_choose.get("who_receive").get_balance(),have_debts=person_choose.get("who_receive").get_have_debts())
+        return service_pb2.PersonResponse(person_send=person_send,value=request.value,person_receive=person_receive, message=person_choose.get("message"))
+         
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
